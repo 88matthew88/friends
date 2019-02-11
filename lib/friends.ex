cdefce1b55410830c25689ac1a457ccf957bc734
88matthew88 <mmcfarlane1995@gmail.com>
2019-02-11 15:07:33 -0500

    update
    change how i write to db

--- lib/friends.ex
+++ lib/friends.ex
@@ -14,24 +14,40 @@ defmodule Friends do
   """
 
 
+
         person = %Friends.Person{}
 
-        def read do
-          File.stream!("file.csv")
-          |> (CSV.decode!)
-          |> Enum.each(fn
-                  {:ok, [fname, lname, age]} ->
-                    Person.changeset(%Person{},
-                          %{Firstname: fname,
-                            Lastname: lname,
-                            age: age})
-                    |>Repo.insert
+#        def read do
+#          File.stream!("file.csv")
+#          |> (CSV.decode!)
+#          |> Enum.each(fn
+#                  {:ok, [fname, lname, age]} ->
+#                    Person.changeset(%Person{},
+#                          %{Firstname: fname,
+#                            Lastname: lname,
+#                            age: age}
+#                        )
+#                    |>Friends.Repo.insert
+#
+#                      end
+#          end
 
-        end   
+    def import_from_csv(csv_path) do
+      Agent.start_link(fn -> %{departments: %HashDict{}, communes: []} end, name: __MODULE__)
 
+      File.stream!(Path.expand(csv_path))
+      |> CSV.decode(separator: ?;, headers: true)
+      |> Stream.each(fn row ->
+        _process_csv_row(row, agent)
+      end )
+      |> Stream.run
+    end
 
-        end
+    defp _process_csv_row(row, agent) do
+      # TODO
+    end
 
+#end
 
 
     def results do
